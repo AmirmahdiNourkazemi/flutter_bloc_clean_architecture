0b9ce3c976285b8b9f72994779293ba126878118
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2024-08-09 10:27:37 +0330

    feat: clean architecture folders created

---
+++ lib/core/main_wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainWrapper extends StatelessWidget {
+  const MainWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
